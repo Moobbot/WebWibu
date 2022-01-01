269b09063b6d1d074426a952e4fdbb24137c829b
Moobbot <90234479+Moobbot@users.noreply.github.com>
2022-01-01 13:16:29 +0700

    Update DBMS

--- SQL/TaoBang.sql
+++ SQL/TaoBang.sql
@@ -7,58 +7,51 @@ USE DB_Wibu;
 GO
 
 --Tạo bảng
---Tên bảng:  Viết hoa chữ cái đầu của từ Vd: NhanVien
---Thuộc tính: Viết in ví dụ MANHANVIEN
+--Tên bảng:  Viết hoa chữ cái đầu của từ Vd: NHANVIEN
+--Thuộc tính: Viết in ví dụ Manhanvien
 --Các loại mã cài đặt IDENTITY(1,1)
 
 --Tài khoản
-CREATE TABLE TaiKhoan(
-	MATAIKHOAN INT IDENTITY(1,1) NOT NULL,
-	TENTAIKHOAN CHAR(20) NOT NULL,
-	MATKHAU NVARCHAR(20) NOT NULL,
-	NGAYTAO DATETIME,
-	CAPDO TINYINT NOT NULL,
-	TRANGTHAI BIT
+CREATE TABLE TAIKHOAN(
+	Mataikhoan INT IDENTITY(1,1) NOT NULL,
+	Tentaikhoan CHAR(20) NOT NULL,
+	Matkhau NVARCHAR(20) NOT NULL,
+	Ngaytao DATETIME,
+	Capdo TINYINT NOT NULL,
+	Trangthai BIT
 )
+
 -- Cấp độ: Có 3 cấp độ. 1.Quản lý, 2.Nhân viên, 3. Khách hàng
 -- Tài khoản khoản Quản lý cấp độ 1. Chỉ có  1 tài khoản là
--- TENTAIKHOAN: admin, MATKHAU: admin, CAPDO: 1
 -- Tạo tài khoản do quản lý tạo thì Cấp độ là 2.
 -- Tài khoản đăng ký thì Cấp độ là 3.
 
 -- Bảng nhân viên 
-CREATE TABLE NhanVien (
-  MANHANVIEN INT IDENTITY(1,1) NOT NULL,
-  HOTEN NVARCHAR(50) NOT NULL,
-  NGAYSINH DATE NOT NULL,
-  NGAYBATDAULAMVIEC DATE NOT NULL,
-  DIACHI NVARCHAR(100),
-  DIENTHOAI CHAR(11) NOT NULL,
-  LUONGCOBAN MONEY NOT NULL,
-  TIENTHUONG MONEY,
-  MATAIKHOAN INT NOT NULL
+CREATE TABLE NHANVIEN (
+  Manhanvien INT IDENTITY(1,1) NOT NULL,
+  Hoten NVARCHAR(50) NOT NULL,
+  Ngaysinh DATE NOT NULL,
+  Ngaybatdaulamviec DATE NOT NULL,
+  Diachi NVARCHAR(100),
+  Dienthoai CHAR(11) NOT NULL,
+  Luongcoban MONEY NOT NULL,
+  Tienthuong MONEY,
+  Mataikhoan INT NOT NULL
 )
---DROP TABLE dbo.NHANVIEN
---MANHANVIEN IDENTITY(1,1)
---NGAYBATDAULAMVIEC DEFAULT getdate()
---LUONGCOBAN DEFAULT 5.000.000
---TIENTHUONG DEFAULT 0 --Tiền thưởng không quá 25% lương
---ALTER TABLE dbo.NHANVIEN
---ADD DEFAULT (5000000) FOR LUONGCOBAN
 
---Nên có thông tin quả lý không?
+--Ngaybatdaulamviec getdate()
+--Luongcoban <= 5.000.000
+--Tienthuong DEFAULT 0 --Tiền thưởng không quá 25% lương
 
 --Bảng Khách hàng
-CREATE TABLE KhachHang (
-  MAKHACHHANG INT IDENTITY(1,1) NOT NULL,
-  HOTEN nvarchar(50) NOT NULL,
-  DIACHI nvarchar(100),
-  EMAIL varchar(255),
-  DIENTHOAI CHAR(11) NOT NULL,
-  MATAIKHOAN INT NOT NULL
+CREATE TABLE KHACHHANG (
+  Makhachhang INT IDENTITY(1,1) NOT NULL,
+  Hoten NVARCHAR(50) NOT NULL,
+  Diachi NVARCHAR(100),
+  Email VARCHAR(255),
+  Dienthoai CHAR(11) NOT NULL,
+  Mataikhoan INT NOT NULL
 )
-
---MAKHACHHANG IDENTITY(1,1)
 
 --Bảng Loại món ăn
 --CREATE TABLE LoaiMonAn (
@@ -67,17 +60,19 @@ CREATE TABLE KhachHang (
 --)
 
 -- Bảng Món ăn
-CREATE TABLE MonAn (
-  MAMONAN INT IDENTITY(1,1) NOT NULL,
-  TENMONAN NVARCHAR(50) NOT NULL,
-  HINHANH NVARCHAR(100),
-  SOLUONG INT NOT NULL,
-  GIATHANHPHAM MONEY NOT NULL, --Là giá khi hoàn thiện món(gồm giá nguyên liệu, giá nhân công)
-  HANSUDUNG DATE NOT NULL
+CREATE TABLE MONAN (
+  Mamonan INT IDENTITY(1,1) NOT NULL,
+  Tenmonan NVARCHAR(50) NOT NULL,
+  Hinhanh NVARCHAR(100),
+  Mota NTEXT,
+  Soluong INT NOT NULL,
+  Giathanhpham MONEY NOT NULL,
+  Ngaynhap DATE,
+  Hansudung DATE NOT NULL
 )
 
 -- Bảng Đơn đặt hàng
-CREATE TABLE DonDatHang (
+CREATE TABLE DONDATHANG (
   SOHOADON INT IDENTITY(1,1) NOT NULL,
   MAKHACHHANG INT NOT NULL,
   MANHANVIEN INT NOT NULL,
@@ -93,64 +88,76 @@ CREATE TABLE DonDatHang (
 
 -- Bảng chi tiết đặt hàng
 
-CREATE TABLE ChiTietDatHang (
-  SOHOADON INT NOT NULL,
-  MAMON INT NOT NULL,
-  GIABAN MONEY NOT NULL,
-  SOLUONG INT NOT NULL,
-  MUCGIAGIAM MONEY
+CREATE TABLE CHITIETDATHANG (
+  Sohoadon INT NOT NULL,
+  Mamon INT NOT NULL,
+  Giaban MONEY,
+  Soluong INT,
+  Mucgiagiam MONEY
 )
 -- Giá bán = 150% Giá thành phẩm
 
---điều kiện
---Thêm thời gian tạo mặc định tài khoản
-ALTER TABLE TaiKhoan
-ADD DEFAULT getdate() FOR NGAYTAO;
-
---Thêm thời gian mặc định đặt đơn 
-ALTER TABLE DonDatHang
-ADD DEFAULT getdate() FOR THOIGIANDATDON;	
-
---Thêm thời gian mặc định ngày bắt đầu làm việc
-ALTER TABLE NhanVien
-ADD DEFAULT getdate() FOR NGAYBATDAULAMVIEC;	
-
---Thêm trạng thái mặc định khi tạo tài khoản
-
-ALTER TABLE TaiKhoan
-ADD DEFAULT 0 FOR TRANGTHAI;
+--------------------------Điều kiện---------------------------------------
+--Thêm thời gian mặc định tạo tài khoản
+ALTER TABLE TAIKHOAN
+ADD DEFAULT getdate() FOR Ngaytao;
 
 --Thêm điều kiện check cấp độ: 1-ADMIN; 2-Nhân viên; 3: Khách hàng
-ALTER TABLE TaiKhoan
-ADD CONSTRAINT ck_capdo CHECK(CAPDO BETWEEN 1 AND 3);
+ALTER TABLE TAIKHOAN
+ADD CONSTRAINT ck_capdo CHECK(Capdo BETWEEN 1 AND 3);
 
---Tạo khóa chính
-ALTER TABLE TaiKhoan
-ADD CONSTRAINT pk_taikhoan PRIMARY KEY (MATAIKHOAN);
-ALTER TABLE NhanVien
-ADD CONSTRAINT pk_nhanvien PRIMARY KEY (MANHANVIEN);
-ALTER TABLE KhachHang
-ADD CONSTRAINT pk_khachhang PRIMARY KEY (MAKHACHHANG);
-ALTER TABLE MonAn
-ADD CONSTRAINT pk_monan PRIMARY KEY (MAMONAN);
-ALTER TABLE DonDatHang
+--Thêm trạng thái mặc định khi tạo tài khoản
+ALTER TABLE TAIKHOAN
+ADD DEFAULT 0 FOR Trangthai;
+
+--Thêm trạng thái mặc định tiền thưởng là 0 khi thêm nhân viên
+ALTER TABLE NHANVIEN
+ADD DEFAULT 0 FOR Tienthuong;
+
+--Thêm thời gian mặc định ngày bắt đầu làm việc
+ALTER TABLE NHANVIEN
+ADD DEFAULT getdate() FOR Ngaybatdaulamviec;
+
+--Thêm thời gian mặc định ngày nhập món ăn
+ALTER TABLE MONAN
+ADD DEFAULT getdate() FOR Ngaynhap;
+
+--Thêm thời gian mặc định đặt đơn
+ALTER TABLE DONDATHANG
+ADD DEFAULT getdate() FOR Thoigiandatdon;
+
+--Thêm mức giá giảm mặc định là 0
+ALTER TABLE CHITIETDATHANG
+ADD DEFAULT 0 FOR Mucgiagiam;
+
+-------------------------Tạo khóa chính---------------------------------------
+ALTER TABLE TAIKHOAN
+ADD CONSTRAINT pk_taikhoan PRIMARY KEY (Mataikhoan);
+ALTER TABLE NHANVIEN
+ADD CONSTRAINT pk_nhanvien PRIMARY KEY (Manhanvien);
+ALTER TABLE KHACHHANG
+ADD CONSTRAINT pk_khachhang PRIMARY KEY (Makhachhang);
+ALTER TABLE MONAN
+ADD CONSTRAINT pk_monan PRIMARY KEY (Mamonan);
+ALTER TABLE DONDATHANG
 ADD CONSTRAINT pk_dondathang PRIMARY KEY (SoHoaDon);
-ALTER TABLE ChiTietDatHang
-ADD CONSTRAINT pk_chitietdathang PRIMARY KEY (SOHOADON, MAMON);
+ALTER TABLE CHITIETDATHANG
+ADD CONSTRAINT pk_chitietdathang PRIMARY KEY (Sohoadon, Mamon);
 
-ALTER TABLE NhanVien
-ADD CONSTRAINT fk_nhanvien_taikhoan FOREIGN KEY (MATAIKHOAN) REFERENCES TaiKhoan(MATAIKHOAN);
-ALTER TABLE KhachHang
-ADD CONSTRAINT fk_khachhang_taikhoan FOREIGN KEY (MATAIKHOAN) REFERENCES TaiKhoan(MATAIKHOAN);
-ALTER TABLE DonDatHang
-ADD CONSTRAINT fk_dondathang_khachhang FOREIGN KEY (MAKHACHHANG) REFERENCES KhachHang(MAKHACHHANG);
-ALTER TABLE DonDatHang
-ADD CONSTRAINT fk_dondathang_nhanvien FOREIGN KEY (MANHANVIEN) REFERENCES NhanVien(MANHANVIEN);
-ALTER TABLE ChiTietDatHang
-ADD CONSTRAINT fk_chitietdathang_dondathang FOREIGN KEY (SOHOADON) REFERENCES DonDatHang(SOHOADON);
-ALTER TABLE ChiTietDatHang
-ADD CONSTRAINT fk_chitietdathang_monan FOREIGN KEY (MAMON) REFERENCES MonAn(MAMONAN);
-
+-------------------------Tạo khóa ngoại---------------------------------------
+ALTER TABLE NHANVIEN
+ADD CONSTRAINT fk_nhanvien_taikhoan FOREIGN KEY (Mataikhoan) REFERENCES TAIKHOAN(Mataikhoan);
+ALTER TABLE KHACHHANG
+ADD CONSTRAINT fk_khachhang_taikhoan FOREIGN KEY (Mataikhoan) REFERENCES TAIKHOAN(Mataikhoan);
+ALTER TABLE DONDATHANG
+ADD CONSTRAINT fk_dondathang_khachhang FOREIGN KEY (Makhachhang) REFERENCES KHACHHANG(Makhachhang);
+ALTER TABLE DONDATHANG
+ADD CONSTRAINT fk_dondathang_nhanvien FOREIGN KEY (Manhanvien) REFERENCES NHANVIEN(Manhanvien);
+ALTER TABLE CHITIETDATHANG
+ADD CONSTRAINT fk_chitietdathang_dondathang FOREIGN KEY (Sohoadon) REFERENCES DONDATHANG(Sohoadon);
+ALTER TABLE CHITIETDATHANG
+ADD CONSTRAINT fk_chitietdathang_monan FOREIGN KEY (Mamon) REFERENCES MONAN(Mamonan);
+---------------------------------------------------------------------------------
 SELECT * FROM ChiTietDatHang
 SELECT * FROM KhachHang
 SELECT * FROM DonDatHang
