0fe8f1356a64b79332e23fb87a329f646cae8a58
Moobbot <90234479+Moobbot@users.noreply.github.com>
2022-01-01 15:08:28 +0700

    Chia khung

--- reuse/header.php
+++ reuse/header.php
@@ -6,6 +6,13 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Wibu</title>
+    <!-- Bootstrap 5 -->
+    <!-- Link css -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <!-- Link fontawesome -->
+    <script src="https://kit.fontawesome.com/a4208f5cfb.js" crossorigin="anonymous"></script>
+
 </head>
 
 <body>
