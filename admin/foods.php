4f756586663e6737659b55f81639ceaddd612d46
loofeht <phu832001@.com>
2022-01-04 19:55:48 +0700

    aaa

--- admin/foods.php
+++ admin/foods.php
@@ -9,7 +9,7 @@
         <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#add-food-modal"><i class="fas fa-plus"></i>Thêm
             món ăn</button>
 
-        <div class="modal fade" id="add-food-modal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <!-- <div class="modal fade" id="add-food-modal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
             <div class="modal-dialog">
                 <div class="modal-content">
                     <div class="modal-header">
@@ -46,7 +46,53 @@
                 </div>
 
             </div>
+        </div> -->
+
+        <div class="modal fade" id="add-food-modal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLabel">Thêm món ăn</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        <div class="col-md-6 me-auto">
+                            <div class="form-floating mb-3">
+                                <input type="text" class="form-control" id="floatingInput" placeholder="">
+                                <label for="floatingInput">Tên món</label>
+                            </div>
+                            <div class="form-floating mb-3">
+                                <input type="text" class="form-control" id="floatingInput" placeholder="">
+                                <label for="floatingInput">Thành phần</label>
+                            </div>
+                            <div class="form-floating mb-3">
+                                <input type="text" class="form-control" id="floatingInput" placeholder="">
+                                <label for="floatingInput">Đơn giá</label>
+                            </div>
+                            <div class="form-floating mb-3">
+                                <input type="number" class="form-control" id="floatingInput" placeholder="">
+                                <label for="floatingInput">Số lượng</label>
+                            </div>
+                            <div class="form-floating mb-3">
+                                <input type="text" class="form-control" id="floatingInput" placeholder="">
+                                <label for="floatingInput">Mô tả</label>
+                            </div>
+                        </div>
+                        <div class="col-md-6 me-auto">
+
+                        </div>
+                    </div>
+
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-primary" onclick="funcAddSuccess()">Đồng ý</button>
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Quay lại</button>
+                    </div>
+                </div>
+
+            </div>
         </div>
+
+
     </div>
 
 
