f986a5f8ee67f27848d2b6eac17bc36b7fe4de8a
Moobbot <ngotam24082001@gmai.com>
2022-01-09 20:59:02 +0700

    modal

--- Admin/accounts.php
+++ Admin/accounts.php
@@ -7,38 +7,6 @@
 
         <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#add-modal"><i class="fas fa-user-plus"></i>Thêm tài khoản</button>
 
-        <div class="modal fade" id="add-modal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-            <div class="modal-dialog">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h5 class="modal-title" id="exampleModalLabel">Thêm tài khoản</h5>
-                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                    </div>
-                    <div class="modal-body">
-                        <div class="form-floating mb-3">
-                            <input type="text" class="form-control" id="floatingName" placeholder="abc">
-                            <label for="floatingName">Họ và Tên</label>
-                        </div>
-                        <div class="form-floating mb-3">
-                            <input type="email" class="form-control" id="floatingUsername" placeholder="name@example.com">
-                            <label for="floatingUsername">Tên đăng nhập</label>
-                        </div>
-                        <div class="form-floating">
-                            <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
-                            <label for="floatingPassword">Mật khẩu</label>
-                        </div>
-                        <div class="form-floating">
-                            <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
-                            <label for="floatingPassword">Nhập lại mật khẩu</label>
-                        </div>
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-primary" onclick="funcAdd()">Đồng ý</button>
-                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Quay lại</button>
-                    </div>
-                </div>
-            </div>
-        </div>
     </div>
 
     <div class="container pb-5" style="min-height: 65vh; height: auto; max-height: 105vh;">
@@ -49,8 +17,6 @@
                     <th class="col">Tên tài khoản</th>
                     <th class="col-1">Mật khẩu</th>
                     <th>Họ tên</th>
-                    <th class="col-1">Ngày bắt đầu làm việc</th>
-                    <th>Địa chỉ</th>
                     <th class="col-1">Điện thoại</th>
                     <th>Hành động</th>
                 </tr>
@@ -61,7 +27,7 @@
                 //* B1: Gọi config
                 include '../config/constants.php';
                 //* B2: Truy vấn
-                $sql = "SELECT taikhoan.mataikhoan, tentaikhoan, matkhau,hoten, ngaybatdaulamviec, diachi, dienthoai  FROM taikhoan, nhanvien WHERE taikhoan.mataikhoan = nhanvien.mataikhoan AND TAIKHOAN.Capdo = 2";
+                $sql = "SELECT taikhoan.mataikhoan, tentaikhoan, matkhau,hoten, dienthoai  FROM taikhoan, nhanvien WHERE taikhoan.mataikhoan = nhanvien.mataikhoan AND TAIKHOAN.Capdo = 2";
                 // $sql = "SELECT mataikhoan, tentaikhoan, matkhau, hoten, ngaybatdaulamviec, diachi, dienthoai  FROM vw_nhanvien_thongtin as a, vw_nhanvien_thongtin as b  WHERE a.mataikhoan = b.mataikhoan";
             
                 // Lưu kết quả trả về $result
@@ -78,8 +44,6 @@
                                 <td class="col"><?php echo $row['tentaikhoan']; ?></td>
                                 <td class="col-1"><?php echo $row['matkhau']; ?></td>
                                 <td class="col-2"><?php echo $row['hoten']; ?></td>
-                                <td class="col-1"><?php echo $row['ngaybatdaulamviec']; ?></td>
-                                <td class="col-2"><?php echo $row['diachi']; ?></td>
                                 <td class="col"><?php echo $row['dienthoai']; ?></td>
                                 <td class="d-flex justify-content-evenly">
                                     <button type="button" class="btn btn-warning" data-bs-toggle="modal" data-bs-target="#passchange">Sửa</button>
@@ -96,7 +60,37 @@
                 ?>
             </tbody>
         </table>
-
+        <!-- Modal thêm tài khoản -->
+        <div class="modal fade" id="add-modal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="exampleModalLabel">Thêm tài khoản</h5>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                            <div class="form-floating mb-3">
+                                <input type="text" class="form-control id" placeholder="" required>
+                                <label>Tên tài khoản</label>
+                            </div>
+                            <div class="form-floating">
+                                <input type="password" class="form-control pass" placeholder="" required>
+                                <label>Mật khẩu</label>
+                            </div>
+                            <div class="form-floating">
+                                <input type="password" class="form-control pass2" placeholder="" required>
+                                <label>Nhập lại mật khẩu</label>
+                            </div>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-primary" id="add">Đồng ý</button>
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Quay lại</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Modal sửa tài khoản -->
         <div class="modal fade" id="passchange" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
             <div class="modal-dialog">
                 <div class="modal-content">
@@ -159,8 +153,37 @@
         }
     }
 
-    function funcAdd() {
-        location.reload()
-        alert("Thêm tài khoản thành công!")
-    }
+</script>
+<!-- Check lỗi -->
+<script>
+    $(document).ready(function() {
+        $('#add').click(function() {
+            $id = $('.id').val();
+            $pass = $('.pass').val();
+            $pass2 = $('.pass2').val();
+            if ($id == "" || $pass == "" || $pass2 == "") {
+                alert("Vui lòng nhập đủ thông tin");
+            } else if ($pass != $pass2) {
+                alert("Mật khẩu nhập lại không đúng");
+            } else {
+                $.ajax({
+                    type: "post",
+                    url: "./process/add-account.php",
+                    data: {
+                        id: $id,
+                        pass: $pass,
+                        pass2: $pass2
+                    },
+                    success: function(response) {
+                        if (response == "success") {
+                            alert("Thêm tài khoản thành công");
+                            location.reload()
+                        } else {
+                            alert("Thêm thất bại");
+                        }
+                    }
+                });
+            }
+        })
+    });
 </script>
