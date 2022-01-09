40b51c2cf06aae88479e564b22af8d82414e0995
loofeht <phu832001@.com>
2022-01-09 20:01:56 +0700

    them tk, monan, bảng nhân viên

--- admin/emp.php
+++ admin/emp.php
@@ -3,19 +3,19 @@
 <!-- Start Menu Admin -->
 <div class="container-fluid custom-background">
     <div class="container pb-5">
-        <h3 class="pt-5 pb-4 fw-bold">Quản lý tài khoản</h3>
-
-        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#add-modal"><i class="fas fa-user-plus"></i>Thêm tài khoản</button>
+        <h3 class="pt-5 pb-4 fw-bold">Quản lý nhân viên</h3>
+        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#add-modal"><i class="fas fa-user-plus"></i>Thêm nhân viên</button>
         <div class="container pt-4" style="min-height: 65vh; height: auto; max-height: 105vh;">
-            <table id="table_acc" class="cell-border table-bordered">
+            <table id="table_emp" class="cell-border table-bordered">
                 <thead>
                     <tr>
                         <th>STT</th>
-                        <th>Tên tài khoản</th>
-                        <th>Mật khẩu</th>
                         <th>Họ tên</th>
-                        <th>Ngày tạo</th>
-                        <th class="col-2">Hành động</th>
+                        <th>Tên tài khoản</th>
+                        <th>Ngày bắt đầu làm việc</th>
+                        <th>Địa chỉ</th>
+                        <th>Điện thoại</th>
+                        <th>Hành động</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -24,7 +24,7 @@
                     //* B1: Gọi config
                     include '../config/constants.php';
                     //* B2: Truy vấn
-                    $sql = "SELECT taikhoan.mataikhoan, tentaikhoan, matkhau, hoten, ngaytao FROM taikhoan, nhanvien WHERE taikhoan.mataikhoan = nhanvien.mataikhoan";
+                    $sql = "SELECT manhanvien, hoten, tentaikhoan, ngaybatdaulamviec, diachi, dienthoai  FROM taikhoan, nhanvien WHERE taikhoan.mataikhoan = nhanvien.mataikhoan";
 
                     //? lưu kết quả trả về $result
                     $result = mysqli_query($conn, $sql);
@@ -34,11 +34,12 @@
                         while ($row = mysqli_fetch_assoc($result)) :
                     ?>
                             <tr>
-                                <th scope="row"><?php echo $row['mataikhoan']; ?></th>
-                                <td><?php echo $row['tentaikhoan']; ?></td>
-                                <td><?php echo $row['matkhau']; ?></td>
+                                <th scope="row"><?php echo $row['manhanvien']; ?></th>
                                 <td><?php echo $row['hoten']; ?></td>
-                                <td><?php echo $row['ngaytao']; ?></td>
+                                <td><?php echo $row['tentaikhoan']; ?></td>
+                                <td><?php echo $row['ngaybatdaulamviec']; ?></td>
+                                <td><?php echo $row['diachi']; ?></td>
+                                <td><?php echo $row['dienthoai']; ?></td>
                                 <td class="d-flex justify-content-evenly">
                                     <button type="button" class="btn btn-warning" data-bs-toggle="modal" data-bs-target="#passchange">Sửa</button>
                                     <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#delete" onclick="funcDel()">Xóa</button>
@@ -57,21 +58,44 @@
                 <div class="modal-dialog">
                     <div class="modal-content">
                         <div class="modal-header">
-                            <h5 class="modal-title" id="exampleModalLabel">Thêm tài khoản</h5>
+                            <h5 class="modal-title" id="exampleModalLabel">Thêm nhân viênhân viên</h5>
                             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                         </div>
                         <div class="modal-body">
                             <div class="form-floating mb-3">
-                                <input type="text" class="form-control id" placeholder="" required>
+                                <input type="text" class="form-control" id="name" placeholder="" required>
+                                <label>Họ và tên</label>
+                            </div>
+                            <div class="form-floating mb-3">
+                                <select id="id" class="form-select form-select-sm mb-3" required>
+                                    <!-- Lấy dữ liệu từ database -->
+                                    <?php
+                                    //? mở kết nối
+                                    include '../config/config.php';
+                                    $sql = "SELECT * FROM taikhoan";
+                                    $result = mysqli_query($conn, $sql);
+                                    //? xác thực
+                                    if (mysqli_num_rows($result) > 0) {
+                                        while ($row = mysqli_fetch_assoc($result)) {
+                                            echo '<option value="' . $row['mataikhoan'] . '">' . $row['tentaikhoan'] . '</option>';
+                                        }
+                                    }
+                                    mysqli_close($conn);
+                                    ?>
+                                </select>
                                 <label>Tên tài khoản</label>
                             </div>
-                            <div class="form-floating">
-                                <input type="password" class="form-control pass" placeholder="" required>
-                                <label>Mật khẩu</label>
+                            <div class="form-floating mb-3">
+                                <input type="date" class="form-control" placeholder="" required>
+                                <label>Ngày bắt đầu làm việc</label>
                             </div>
-                            <div class="form-floating">
-                                <input type="password" class="form-control pass2" placeholder="" required>
-                                <label>Nhập lại mật khẩu</label>
+                            <div class="form-floating mb-3">
+                                <input type="text" class="form-control" placeholder="" required>
+                                <label>Địa chỉ</label>
+                            </div>
+                            <div class="form-floating mb-3">
+                                <input type="text" class="form-control" placeholder="" required>
+                                <label>Điện thoại</label>
                             </div>
                         </div>
                         <div class="modal-footer">
@@ -104,8 +128,8 @@
                             <label>Mật khẩu mới</label>
                         </div>
                         <div class="form-floating">
-                            <input type="password" class="form-control" placeholder="">
-                            <label>Xác nhận mật khẩu</label>
+                            <input type="password" class="form-control" id="floatingPassword" placeholder="">
+                            <label for="floatingPassword">Xác nhận mật khẩu</label>
                         </div>
                     </div>
                     <div class="modal-footer">
@@ -123,7 +147,7 @@
 
 <script>
     $(document).ready(function() {
-        $('#table_acc').DataTable();
+        $('#table_emp').DataTable();
     });
 </script>
 
@@ -140,37 +164,4 @@
             alert("Cập nhật thành công!");
         }
     }
-</script>
-
-<script>
-    $(document).ready(function() {
-        $('#add').click(function() {
-            $id = $('.id').val();
-            $pass = $('.pass').val();
-            $pass2 = $('.pass2').val();
-            if ($id == "" || $pass == "" || $pass2 == "") {
-                alert("Vui lòng nhập đủ thông tin");
-            } else if ($pass != $pass2) {
-                alert("Mật khẩu nhập lại không đúng");
-            } else {
-                $.ajax({
-                    type: "post",
-                    url: "./process/add-account.php",
-                    data: {
-                        id: $id,
-                        pass: $pass,
-                        pass2: $pass2
-                    },
-                    success: function(response) {
-                        if (response == "success") {
-                            alert("Thêm thành công");
-                            location.reload()
-                        } else {
-                            alert("Thêm thất bại");
-                        }
-                    }
-                });
-            }
-        })
-    });
 </script>
